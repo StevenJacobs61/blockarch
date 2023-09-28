22c56ad32a9360f32e7453ef384c82a285bb35fe
StevenJAcobs61 <stevenjacobs61@gmail.com>
2023-09-28 15:48:16 +0100

    background

--- src/components/home/landingPage/perks.jsx
+++ src/components/home/landingPage/perks.jsx
@@ -4,11 +4,15 @@ import CardGrid from '../../cardGrid'
 const Perks = () => {
   return (
     <div className='perks_container'>
+      <div className="perks_topContainer">
         <h1 className='gettingStarted_hdr'>The process</h1>
         <h2 className='gettingStarted_subtext'>Gain Value From Day One</h2>
+      </div>
+      <div className="perks_contentContainer">
         <div className='perks_gridContainer'>
             <CardGrid/>
         </div>
+      </div>
     </div>
   )
 }
