054712f47f82ee0acf2059069440a176636cab81
StevenJAcobs61 <stevenjacobs61@gmail.com>
2024-02-23 19:11:57 +0000

    margin home

--- src/components/home/landingPage/gettingStarted.jsx
+++ src/components/home/landingPage/gettingStarted.jsx
@@ -1,19 +1,20 @@
-import React from 'react'
-import CardAreaTwo from '../../cardAreaTwo'
-import '../../../styles/gettingStarted.scss'
+import CardAreaTwo from "../../cardAreaTwo";
+import "../../../styles/gettingStarted.scss";
 
 const GettingStarted = () => {
   return (
-    <div className='gettingStarted_container'>
+    <div className="gettingStarted_container">
       <div className="gettingStarted_topContainer">
-        <h1 className='gettingStarted_hdr'>Getting Started</h1>
-        <h2 className='gettingStarted_subtext'>Design, develop and deploy blockchain applications with confidence</h2>
+        <h1 className="gettingStarted_hdr">Getting Started</h1>
+        <h2 className="gettingStarted_subtext">
+          Design, develop and deploy blockchain applications with confidence
+        </h2>
       </div>
       <div className="gettingStarted_contentContainer">
-        <CardAreaTwo/>
+        <CardAreaTwo />
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default GettingStarted
+export default GettingStarted;
